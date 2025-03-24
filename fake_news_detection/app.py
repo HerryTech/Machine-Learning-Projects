5299b61704309f78d81d9786642af5efa855ccc2
Blessing <blessingilesanmi21@gmail.com>
2025-03-24 14:06:19 +0100

    update

---
+++ fake_news_detection/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from src.utils import load_model
+
+model = load_model("model/model.pkl")
+vectorizer = load_model("model/vectorizer.pkl")
+
+#streamlit UI
+st.title("Fake New Dectection App")
+st.write("Enter a news article and check if it's FAKE or REAL.")
+
+#input text area
+news_text = st.text_area("Enter news text:", "")
+
+if st.button("Check News"):
+    if news_text:
+        text_tfidf = vectorizer.transform([news_text])
+        prediction = model.predict(text_tfidf)[0]
+        if prediction[0] == 1:
+            st.error("🚨 This news article is FAKE!")
+        else:
+            st.success("✅ This news article is REAL!")
+    else:
+        st.warning("⚠️ Please enter some text.")
+
--- fake_news_detection/app/app.py
+++ fake_news_detection/app/app.py
@@ -15,10 +15,10 @@ vectorizer = load_model(VECTORIZER)
 def home():
     return render_template("index.html")
 
-@app.route("/predict", method = ["POST"])
+@app.route("/predict", methods = ["POST"])
 def predict():
     if request.method == "POST":
-        news_text = request.form("news_text")
+        news_text = request.form["news_text"]
         text_tfidf = vectorizer.transform([news_text])
         prediction = model.predict(text_tfidf)[0]
         result = "Real News" if prediction == 1 else "Fake News"
