c3de04eb0754b2bc7b0cc28866858dffc4c2bfbc
Blessing <blessingilesanmi21@gmail.com>
2025-03-26 12:23:21 +0100

    update app

--- fake_news_detection/app/app.py
+++ fake_news_detection/app/app.py
@@ -1,13 +1,7 @@
 import os
 import sys
+import joblib
 import streamlit as st
-
-try:
-    import joblib
-except ModuleNotFoundError:
-    st.warning("⚠️ joblib not found. Installing now...")
-    os.system("pip install joblib")
-    import joblib
 
 # Define the paths
 model_path = "model/model.pkl"
