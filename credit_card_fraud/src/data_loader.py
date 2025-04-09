eeca222a5045888c050909d0869237da14f874bb
Blessing <blessingilesanmi21@gmail.com>
2025-04-09 14:09:10 +0100

    update

--- credit_card_fraud/src/data_loader.py
+++ credit_card_fraud/src/data_loader.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 import sys
+from sklearn.preprocessing import StandardScaler
 from src.logger import get_logger
 from src.exception import CustomException
 from src.utils import save_data
 
 logger = get_logger(__name__)
 
-def load_clean_data(data_path, output_path):
+def load_clean_data(data_path):
     """load and clean the dataset"""
     try:
         logger.info("Loading dataset...")
@@ -20,19 +21,28 @@ def load_clean_data(data_path, output_path):
         df.drop_duplicates(inplace = True)
         logger.info(f"Dataset loaded and cleaned successfully! Total Dataset: {len(df)}")
 
-        #save cleaned data
-        save_data(df, output_path)
-
         return df
 
     except Exception as e:
         raise CustomException (f"Error loading dataset: {e}", sys)
     
-def preprocessing_and_save():
-    """"""
+def preprocessing_and_save(df, output_path):
+    """Scale Amount and Time Columns and saves the data"""
+    try:
+        logger.info("Starting feature scaling...")
+        feature_to_scale = ["Amount", "Time"]
+        scale = StandardScaler()
+        df[feature_to_scale] = scale.fit_transform(df[feature_to_scale])
+
+        #save cleaned data
+        save_data(df, output_path)
+
+    except Exception as e:
+        raise CustomException(f"Error scaling and saving the data: {e}", sys)
     
 if __name__ == "__main__":
-    load_clean_data("data/raw/creditcard.csv", "data/processed/cleanedcredit.csv")
+    df = load_clean_data("data/raw/creditcard.csv")
+    preprocessing_and_save(df, "data/processed/cleanedcredit.csv")
 
 
 
