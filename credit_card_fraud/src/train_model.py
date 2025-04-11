75b93a62f1f754a151b21c368dec6ab42d9bf06b
Blessing <blessingilesanmi21@gmail.com>
2025-04-11 15:03:15 +0100

    update train_model

--- credit_card_fraud/src/train_model.py
+++ credit_card_fraud/src/train_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sys
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
 from src.logger import get_logger
@@ -9,7 +10,13 @@ logger = get_logger(__name__)
 
 def split_dataset(data_path):
     """Split data into training and testing set"""
-    df = load_data(data_path)
-    X = 
-    logger.info("Data splitting starting...")
-    X_train, y
+    try:
+        df = load_data(data_path)
+        X = df.drop(columns = ["Class"])
+        y = df["Class"]
+        logger.info("Data splitting starting...")
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+        logger.info(f"Data splitting successful! - Total training set: {len(X_train)}")
+
+    except Exception as e:
+        raise CustomException(f"Error in splitting dataset: {e}", sys)
