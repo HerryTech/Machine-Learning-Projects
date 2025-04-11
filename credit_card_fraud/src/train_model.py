632db41063895ac1c3a71f8e81073b40127f8720
Blessing <blessingilesanmi21@gmail.com>
2025-04-11 14:51:02 +0100

    update train_model

--- credit_card_fraud/src/train_model.py
+++ credit_card_fraud/src/train_model.py
@@ -1,3 +1,15 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
+from src.logger import get_logger
+from src.exception import CustomException
+from src.utils import load_data
+
+logger = get_logger(__name__)
+
+def split_dataset(data_path):
+    """Split data into training and testing set"""
+    df = load_data(data_path)
+    X = 
+    logger.info("Data splitting starting...")
+    X_train, y
