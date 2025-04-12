ca9441a0a0ce2aa155baee7d07912a2fe49100a8
Blessing <blessingilesanmi21@gmail.com>
2025-04-12 16:15:43 +0100

    create evaluate script

---
+++ credit_card_fraud/src/evaluate_model.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from src.logger import get_logger
+
+logger = get_logger(__name__)
+
+def evaluate_model():
