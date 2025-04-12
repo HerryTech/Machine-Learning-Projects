8b7c660dcfb735984f00f610421b54e7f0b75ce0
Blessing <blessingilesanmi21@gmail.com>
2025-04-12 16:31:39 +0100

    update evaluate script

--- credit_card_fraud/src/evaluate_model.py
+++ credit_card_fraud/src/evaluate_model.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sys
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from src.logger import get_logger
+from src.exception import CustomException
+from src.train_model import split_dataset
 
 logger = get_logger(__name__)
 
 def evaluate_model():
     """Evaluate the trained model using some metrics"""
     try:
-        
+        split_dataset(data_path)
+
+    except Exception as e:
+        raise CustomException (f"Error in evaluating model: {e}", sys)
