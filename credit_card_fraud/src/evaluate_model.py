2f6246e32bba68f1033a9fedd02337e8eea047be
Blessing <blessingilesanmi21@gmail.com>
2025-04-12 16:57:22 +0100

    update evaluate script

--- credit_card_fraud/src/evaluate_model.py
+++ credit_card_fraud/src/evaluate_model.py
@@ -11,21 +11,32 @@ from src.utils import load_model
 
 logger = get_logger(__name__)
 
-def evaluate_model(data_path, model, model_path):
+def evaluate_model(data_path, model_path):
     """Evaluate the trained model using some metrics"""
     try:
-        X_test, y_test = split_dataset(data_path)
+        _, X_test, _, y_test = split_dataset(data_path)
 
-        load_model(model, model_path)
+        model = load_model(model_path)
 
         #make prediction
-        y_pred = model
+        y_pred = model.predict(X_test)
 
-        #accuracy score
-        accuracy = accuracy_score()
+        #calculate accuracy score
+        accuracy = accuracy_score(y_test, y_pred)
+        logger.info(f"Model Accuracy: {accuracy:.2f}")
+
+        # Display classification report
+        print("Classification Report:\n", classification_report(y_test, y_pred))
+
+        #plot confusion matrix
+        cm = confusion_matrix(y_test, y_pred)
+        sns.heatmap(cm, annot = True, xticklabels = ["Non-Fraud", "Fraud"], yticklabels = ["Non-Fraud", "Fraud"])
+        plt.xlabel("Predicted")
+        plt.ylabel("Actual")
+        plt.title("Confusion Matrix")
 
     except Exception as e:
         raise CustomException (f"Error in evaluating model: {e}", sys)
     
 if __name__ == "__main__":
-    
+    evaluate_model("data/processed/cleanedcredit.csv", "model/model.pkl")
