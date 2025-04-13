4d374f059cf219965d669cd5d507afb69549eacd
Blessing <blessingilesanmi21@gmail.com>
2025-04-13 19:02:53 +0100

    update app

--- credit_card_fraud/app.py
+++ credit_card_fraud/app.py
@@ -7,30 +7,27 @@ st.set_page_config(page_title = "Credit Card Fraud Detector", page_icon="💳")
 st.title("💳Credit Card Fraud Detection")
 st.markdown("Fill in the transaction details below to check for potential fraud.")
 
-#define column names
-columns = ["Time"] + [f"V{i}" for i in range(1, 29)] + ["Amount"]
-
 #dictionary to hold user's input
 sample = {}
 
+#define column names
+columns = ["Time"] + [f"V{i}" for i in range(1, 29)] + ["Amount"]
+
 #create input field
-with st.form("Fraud form"):
-    for col in columns:
-        sample[col] = st.number_input(label = col)
+for col in columns:
+    sample[col] = st.number_input(label = col, value = 0.0, format = "%.4f")
 
-    submitted = st.form_submit_button("🔍 Predit Fraud")
-
-    if submitted:
-        try:
+if st.button("🔍 Predit Fraud"):
+    try:
         #define path and load model
-            model_path = "model/model.pkl"
-            model = load_model(model_path)
-            result, probability = predict_fraud(sample, model_path)
-            st.success(f"Prediction: {result}")
-            st.info(f"Confidence level: {probability:.2%}")
-    
-        except Exception as e:
-            st.error(f"⚠️ Error during prediction: {e}")
+        model_path = "model/model.pkl"
+        model = load_model(model_path)
+        result, probability = predict_fraud(sample, model_path)
+        st.success(f"This is a {result}")
+        st.info(f"Confidence level: {probability:.2%}")
+        
+    except Exception as e:
+        st.error(f"⚠️ Something went wrong: {e}")
 
 
     
