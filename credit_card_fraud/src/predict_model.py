6087d614acc90e7879993c2eb73199df770e2d53
Blessing <blessingilesanmi21@gmail.com>
2025-04-13 16:23:43 +0100

    update predict script

--- credit_card_fraud/src/predict_model.py
+++ credit_card_fraud/src/predict_model.py
@@ -3,3 +3,15 @@ from sklearn.preprocessing import StandardScaler
 from src.logger import get_logger
 from src.exception import CustomException
 from src.utils import load_model
+
+logger = get_logger(__name__)
+
+def make_prediction(df, model_path):
+    """Predict whether a transaction is fraudulent or not"""
+    try:
+        model = load_model(model_path)
+        feature_to_scale = ["Amount", "Time"]
+        scale = StandardScaler()
+        df[feature_to_scale] = scale.fit_transform(df[feature_to_scale])
+
+
