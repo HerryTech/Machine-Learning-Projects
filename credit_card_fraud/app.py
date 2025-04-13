60cfd8dbf783e1c3b10d995d3971f1a1a881b85d
Blessing <blessingilesanmi21@gmail.com>
2025-04-13 17:26:57 +0100

    update app

--- credit_card_fraud/app.py
+++ credit_card_fraud/app.py
@@ -1,2 +1,10 @@
 import joblib
 import streamlit as st
+from src.utils import load_model
+
+#define path and load model
+model_path = "model/model.pkl"
+model = load_model(model_path)
+
+def main():
+    st.title("Credit Card Fraud Detection")
