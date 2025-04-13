10d3e00202c1611a2c951e3372ad5db36d9c0413
Blessing <blessingilesanmi21@gmail.com>
2025-04-13 19:26:04 +0100

    update app

--- credit_card_fraud/app.py
+++ credit_card_fraud/app.py
@@ -7,6 +7,23 @@ st.set_page_config(page_title = "Credit Card Fraud Detector", page_icon="💳")
 st.title("💳Credit Card Fraud Detection")
 st.markdown("Fill in the transaction details below to check for potential fraud.")
 
+# Sidebar information
+with st.sidebar:
+    st.header("🧾 Feature Description")
+    st.markdown("""
+    **Time**: Seconds elapsed between the transaction and the first transaction in the dataset.  
+    **V1 - V28**: Result of a PCA transformation - these are anonymized features.  
+    **Amount**: Transaction amount in USD.  
+    """)
+
+    st.markdown("---")
+    st.markdown("📊 **How to Use:**\n- Fill in the fields on the main page.\n- Click '🔍 Predict Fraud' to run the model.\n- You'll get a prediction with a confidence level.")
+
+    st.markdown("---")
+    st.caption("🛡️ Powered by a machine learning model trained on anonymized credit card data (from kaggle).")
+    st.caption("👩‍💻 App developed by HerryTech (https://github.com/HerryTech)")
+
+
 #dictionary to hold user's input
 sample = {}
 
@@ -20,7 +37,7 @@ for col in columns:
 if st.button("🔍 Predit Fraud"):
     try:
         #define path and load model
-        model_path = "model/model.pkl"
+        model_path = "Credit_card_fraud/model/model.pkl"
         model = load_model(model_path)
         result, probability = predict_fraud(sample, model_path)
         st.success(f"This is a {result}")
