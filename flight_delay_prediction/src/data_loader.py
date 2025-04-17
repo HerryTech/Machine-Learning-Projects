aa9302c9a0743896badc65750d9b3cd52524d7ce
Blessing <blessingilesanmi21@gmail.com>
2025-04-17 21:08:21 +0100

    update data loader script

--- flight_delay_prediction/src/data_loader.py
+++ flight_delay_prediction/src/data_loader.py
@@ -1,9 +1,17 @@
 import pandas as pd
+import sys
 from src.logger import get_logger
+from exception import CustomException
 
 logger = get_logger(__name__)
 
-def load_data(file_path):
+def load_flight_data(file_path):
     """Function to load data"""
-    return pd.read_csv(file_path)
+    try:
+        logger.info(f"Loading data from {file_path}")
+        df = pd.read_csv(file_path)
+        logger.info(f"Shape of data: {df.shape}")
+        return df
+    except Exception as e:
+        raise CustomException(e, sys)
     
